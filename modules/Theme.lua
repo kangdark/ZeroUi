32eaa62396c271bde05629b82c4c6e0d464c4294
kangdark <garzutama@gmail.com>
2026-06-18 20:53:20 +0700

    Create Theme.lua

---
+++ modules/Theme.lua
@@ -0,0 +1,20 @@
+--[[
+    Zero UI
+    Author: KangDark
+]]
+
+return {
+
+    Background = Color3.fromRGB(13,13,13),
+
+    Header = Color3.fromRGB(17,17,17),
+
+    Border = Color3.fromRGB(38,38,38),
+
+    Text = Color3.fromRGB(255,255,255),
+
+    ToggleOn = Color3.fromRGB(0,94,255),
+
+    ToggleOff = Color3.fromRGB(26,26,26)
+
+}
